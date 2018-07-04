8471870150d6c4da5ea098e8b15dba471eb4a01d
rootbmb <rootbmb@gmail.com>
2018-07-04 13:00:45 +0300

    create_conf_dhcp.py dir100 fix

--- create_conf_dhcp.py
+++ create_conf_dhcp.py
@@ -23,7 +23,6 @@ def create_conf(vlan, subnet, switch_conf, file_name, relay):
             for ip in subnet:
                 ip_list.append(ip)
             file.write(switch_conf.format(vlan, ip_list[1], ip_list[-1]))
-            
 
 
 def main():
@@ -33,7 +32,7 @@ def main():
         pass
     if len(argv) < 3 or len(argv) > 5:
         print('''
-		Enter arguments:
+        Enter arguments:
 
                 1. Vlan number  10
                 2. Used Network  192.168.10.64/26
@@ -49,7 +48,7 @@ def main():
 
         Dlink = 'class "vlan{0}port-{1}"{{match if (binary-to-ascii(10, 16, "",  substring(option agent.circuit-id, 2, 2))="{0}" and binary-to-ascii(10, 16, "",  substring(option agent.circuit-id, 4, 2))="{1}")}}\npool {{range {2}; allow members of "vlan{0}port-{1}"}}\n'
         Snr = 'class "vlan{0}port-{1}"{{match if ( binary-to-ascii(10, 16, "",  substring(option agent.circuit-id, 2, 2)) = "{0}") and (binary-to-ascii(10,8,"",suffix(option agent.circuit-id,1)))= "{1}";}}\npool {{range {2}; allow members of "vlan{0}port-{1}";}}\n'
-        dir100 = 'class "vlan{0}"{{match if (binary-to-ascii(10, 16, "",  substring(option agent.circuit-id, 2, 2))="{0}")}}\npool {{range {1} {2}; allow members of "0"; }}\n'
+        dir100 = 'class "vlan{0}"{{match if ( binary-to-ascii(10, 16, "",  substring(option agent.circuit-id, 2, 2)) = "{0}"); }}\npool {{range {1} {2}; allow members of "vlan{0}"; }}\n'
 
         if switch == 'snr':
             create_conf(vlan, subnet, Snr, file_name, 1)
@@ -61,4 +60,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
